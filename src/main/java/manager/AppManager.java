d4e50b98230bd93ab16c55610ad8b8896527fbfe
ksuzh <ksuzha1@gmail.com>
2026-09-21 16:58:23 +0300

    workflow

--- src/main/java/manager/AppManager.java
+++ src/main/java/manager/AppManager.java
@@ -1,5 +1,6 @@
 package manager;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
@@ -14,7 +15,8 @@ public class AppManager {
     @BeforeMethod(alwaysRun = true)
     public void setUp() {
         driver = new ChromeDriver();
-        driver.manage().window().maximize();
+        //driver.manage().window().maximize();
+        driver.manage().window().setSize(new Dimension(1920, 1080));
     }
 
     @AfterMethod(alwaysRun = true)
